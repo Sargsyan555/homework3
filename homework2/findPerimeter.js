07bbab66f3bb9b1e38a0fb3addd6918e77136bf1
Romik Sargsyan <sargsyanromik6@gmail.com>
2023-02-20 21:48:16 +0300

    changes

--- homework2/findPerimeter.js
+++ homework2/findPerimeter.js
@@ -1,17 +1,33 @@
-// 2. Create a function that returns the frequency distribution of an array. This function should return an object, where the keys are the unique elements and the values are the frequency in which those elements occur.
-// getFrequencies(["A", "B", "A", "A", "A"]) ➞ { A: 4, B: 1 }
-// getFrequencies([1, 2, 3, 3, 2]) ➞ { "1": 1, "2": 2, "3": 2 }
-// getFrequencies([true, false, true, false, false]) ➞ { true: 2, false: 3 }
-// getFrequencies([]) ➞ {}
-function getFrequencies(array){
-    return array.reduce((acc, item) => {
-        if(acc[item]){
-            acc[item] = acc[item] + 1
-        }else{
-            acc[item] = 1
-        }
+// 3. Create a function that sorts an array and removes all duplicate items from it.
 
-        return acc
-    }, {})
+// set([1, 3, 3, 5, 5]) ➞ [1, 3, 5]
+// set([4, 4, 4, 4]) ➞ [4]
+// set([5, 7, 8, 9, 10, 15]) ➞ [5, 7, 8, 9, 10, 15]
+//
+// function set(array){
+//     let newArray = []
+//     for(let i = 0; i < array.length; i++){
+//         if(!newArray.includes(array[i])) {
+//             newArray.push(array[i])
+//         }
+//     }
+//     return newArray
+// }
+function set(array){
+    let arr = array.filter((item, index)=>{
+        console.log(array.indexOf(item), index)
+        return array.indexOf(item) === index
+
+    })
+    return arr
 }
-console.log(getFrequencies([true, false, true, false, false]));;
+//
+// function setWithMethod(array) {
+//    return  array.reduce(function (accumulator, currentValue) {
+//        if(accumulator.indexOf(currentValue) === -1){
+//            return accumulator.push(currentValue)
+//        }
+//    }, [])
+// }
+//
+ console.log(set([1,1, 1, 3, 3, 3, 5, 5]) )
